c079ccb5e8af108040cd62f3d04356d58f3126c0
feldon003 <feldonkor04@gmail.com>
2024-03-06 18:40:05 +0000

    updated version

--- src/views/ElectricFuture.vue
+++ src/views/ElectricFuture.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Header />
-    <Mainbglinks msg="Electric Future" vid="/car logo loop 1.mp4" />
+    <Mainbglinks msg="Electric Future" vid="/CW main background.mp4" />
   </div>
 </template>
 
