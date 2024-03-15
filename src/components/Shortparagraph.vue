04c7242f2e4808bb332348f7a82cabd3cbf141fd
feldon003 <feldonkor04@gmail.com>
2024-03-15 14:00:13 +0000

    Added shortparagraph.vue

---
+++ src/components/Shortparagraph.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="paragraph">
+      <div class="content">
+        <h2>{{ msg }}</h2>
+      </div>
+         <div class="date">
+          <p>{{ date }}</p>
+         </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'Shortparagraph',
+    props: ['msg', 'date']
+}
+</script>
+<style scoped>
+.paragraph{
+  height: 60vh;
+  width: 100%;
+  /* background-color: aqua; */
+  display: flex;
+  flex-direction: column;
+  padding: 5% 15%;
+}
+
+.paragraph h2{
+  font-size: 27px;
+}
+.paragraph .date{
+  margin-top: 4vh;
+}
+
+.pictures{
+  height: 30vh;
+}
+</style>
--- src/views/CircularWorld.vue
+++ src/views/CircularWorld.vue
@@ -2,16 +2,23 @@
   <div class="main">
     <Header />
     <mainBgLinks msg="Circular World" vid="/CW main background.mp4"/>
+    <Shortparagraph msg="Sustainability and circular thinking go hand in hand. Technologies and entrepreneurship 
+        help develop solutions that lead us harmoniously into the future. Recycling and conserving resources
+         are an integeral part of this journey. 
+        All under the motto: RE:DUCE, RE:USE, RE:CYCLE, RE:THINK. Join us on our search for the thrends and 
+        drivers that are shaping our future while protecting the environment." date="18 April 2023"/>
   </div>
+  
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
 import MainBgLinks from '@/components/Mainbglinks.vue'
+import Shortparagraph from '@/components/Shortparagraph.vue'
 
 export default {
   name: 'Circular World',
-  components: { Header, MainBgLinks }
+  components: { Header, MainBgLinks, Shortparagraph }
 }
 </script>
 
@@ -22,4 +29,5 @@ export default {
   /* background-color: aqua; */
   position: relative;
 }
+
 </style>
